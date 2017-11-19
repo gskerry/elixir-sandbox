93043f66d1bb0ee0a2b7e75fc9e9cf9f13d40872
gskerry <gregory.skerry@gmail.com>
2017-11-18 20:31:53 -0800

    refactor for image build updates

--- dockerize/container-run.sh
+++ dockerize/container-run.sh
@@ -1,3 +1,3 @@
 
 echo $(pwd)
-docker run -it --rm --name elixir_container -p 4000:4000 -v $(pwd):/home/apps -w /home/apps elixir-proj_img:0.0.1 iex
+docker run -it --rm --name elixir_container -p 4000:4000 -v $(pwd):/home/myapp -w /home/myapp elixir-proj_img:0.0.2 iex
