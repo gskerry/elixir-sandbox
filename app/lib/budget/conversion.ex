49e3e7d9d0135e3fed7ce9023a9ba352c0f3e8bc
gskerry <gregory.skerry@gmail.com>
2017-11-18 17:05:03 -0800

    build in http call

--- app/lib/budget/conversion.ex
+++ app/lib/budget/conversion.ex
@@ -1,5 +1,9 @@
 defmodule Budget.Conversion do
     def euro_to_dollar(amount) do
-        
+        url = "cs-currency-rates.codeschool.com/currency-rates"
+        case HTTPoison.get(url) do
+            {:ok, response} -> parse(response) |> convert(amount)
+            {:error, _} -> "Error fetching rates"
+        end
     end
 end
