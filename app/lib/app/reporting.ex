ee38ebc8b753bf75d67f302465a1beb64661dc52
gskerry <gregory.skerry@gmail.com>
2017-11-18 21:34:00 -0800

    Filtering csv data

--- app/lib/app/reporting.ex
+++ app/lib/app/reporting.ex
@@ -5,9 +5,15 @@ defmodule App.Reporting do
         # File.read("lib/transactions-jan.csv") # returns full tuple
 
         # returns content directly (or error if file is missing... don't have conditional logic to apply to an error tuple, so this works)
-        File.read!("lib/transactions-jan.csv") |> parse
+        File.read!("lib/transactions-jan.csv")
+            |> parse
+            |> filter # account is always the same... drop
     end
     defp parse(string) do
         Nimble.parse_string(string)
+    end
+    defp filter(rows) do
+        # Enum.map(rows, fn(row) -> Enum.drop(row, 1) end) # Enum, standard lib
+        Enum.map(rows, &Enum.drop(&1, 1))
     end
 end
