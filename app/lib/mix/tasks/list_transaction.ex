8445f1614c692c02edd3319d9b5be6e3f1c1ab51
gskerry <gregory.skerry@gmail.com>
2017-11-18 21:16:55 -0800

    add function shortdoc (appears in mix help)

--- app/lib/mix/tasks/list_transaction.ex
+++ app/lib/mix/tasks/list_transaction.ex
@@ -1,6 +1,7 @@
 defmodule Mix.Tasks.ListTransactions do
     use Mix.Task # core module
 
+    @shortdoc "list transactions from CSV file."
     def run(_) do # argument underscore... avoid compiler warnings
         App.Reporting.list_transactions |> IO.inspect
     end
