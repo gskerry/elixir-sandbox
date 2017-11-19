38f4350a89a04a903e2725df2838d6c715c7a99c
gskerry <gregory.skerry@gmail.com>
2017-11-18 21:24:27 -0800

    add nimble library and parse csv

--- app/lib/app/reporting.ex
+++ app/lib/app/reporting.ex
@@ -1,7 +1,12 @@
 defmodule App.Reporting do
     def list_transactions do
-        "Listing tx's..."
+        IO.puts "Listing tx's..."
         # File.read("lib/transactions-jan.csv") # returns full tuple
-        File.read!("lib/transactions-jan.csv") # returns content directly (or error if file is missing... don't have conditional logic to apply to an error tuple, so this works)
+
+        # returns content directly (or error if file is missing... don't have conditional logic to apply to an error tuple, so this works)
+        File.read!("lib/transactions-jan.csv") |> parse
+    end
+    defp parse(string) do
+        NimbleCSV.RFC4180.parse_string(string)
     end
 end
