5770f48acad805e074ed9ff66bc7717107ba3972
gskerry <gregory.skerry@gmail.com>
2017-11-18 21:18:03 -0800

    read csv file

--- app/lib/app/reporting.ex
+++ app/lib/app/reporting.ex
@@ -1,5 +1,7 @@
 defmodule App.Reporting do
     def list_transactions do
         "Listing tx's..."
+        # File.read("lib/transactions-jan.csv") # returns full tuple
+        File.read!("lib/transactions-jan.csv") # returns content directly (or error if file is missing... don't have conditional logic to apply to an error tuple, so this works)
     end
 end
